6d8d1e93469fe36f8e2bb0b07d158e5e3cde3b8f
Will Warren <will.warren04@gmail.com>
2017-06-12 22:33:29 -0400

    testing nav links

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # test_portfolio
+
+## Objectives
+1. To become more familiar with automation using Selenium
+2. To make sure my portfolio page performs correctly through various browers.
+3. To learn and see how I can better write web apps for tests in the future
+
+## Tools
+1. Selenium
+2. Unittest
+
+## How to Run
+Set Virtual Environment
+```
+$ echo layout python3 > .envrc && direnv allow
+```
+Install requirements
+```
+$ pip install -r requirements.txt
+```
+Run Tests
+```
+$ python3 portfolio_test.py
+```
+
+### Testing Plan
+* Test each browser type, Chrome, Firefox, Safari, IE, Opera, Android, iOS
+* In each browser, test:
+  - title
+  - correct link text for desktop
+  - correct link css image class for small screen/mobile
+  - correct link address
+  - each portfolio piece has buttons for its live demo, github, and external
+  sources(optional)
+
+#### Later additions
+1. Test each live demo
+
+#### Author
+[Will Warren](https://willwile4.github.io)
--- portfolio_test.py
+++ portfolio_test.py
@@ -1,18 +1,112 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 import unittest
 import time
 import re
 
 
-class PortfolioTest(unittest.TestCase):
+class PortfolioTestChrome(unittest.TestCase):
 
     def setUp(self):
-        self.browser = webdriver.Chrome()
-        self.addCleanup(self.browser.quit)
+        self.driver = webdriver.Chrome()
+        self.addCleanup(self.driver.quit)
 
-    def testPageTitle(self):
-        self.browser.get('https://willwile4.github.io/')
-        self.assertIn('Will Warren 4', self.browser.title)
+    # def testPageTitle(self):
+    #     '''tests correct page title'''
+    #     self.driver.get('https://willwile4.github.io/')
+    #     self.assertIn('Will Warren 4', self.driver.title)
+
+    # def testDesktopLinkText(self):
+    #     '''tests correct text on large screen (>768px) navigation links'''
+    #     self.driver.get('https://willwile4.github.io/')
+    #     nav_li= [li.text for li in self.driver.find_elements(By.ID,
+    #                                                           'nav-list')]
+    #     nav_li = nav_ul[0].split('\n')
+    #     self.assertIn('GitHub', nav_li)
+    #     self.assertIn('Resume', nav_li)
+    #     self.assertIn('LinkedIn', nav_li)
+    #     self.assertIn('Instagram', nav_li)
+
+    def testGitHubDesktop(self):
+        '''tests github link goes to my GitHub'''
+        self.driver.get('https://willwile4.github.io')
+        nav = self.driver.find_element(By.ID, 'nav-list')
+        print(nav.text, 'line 34')
+
+    # def testMobileLinks(self):
+    #     '''tests correct image on small screen (<768px) navigation links'''
+    #     self.driver.get('https://willwile4.github.io/')
+    #     nav = self.driver.find_element(By.ID, 'mobile-nav')
+    #     print(nav)
+
+    # def testPortfolio(self):
+    #     '''tests portfolio item buttons'''
+    #     pass
+    #
+    # def testAccordian(self):
+    #     '''tests accordian functionality'''
+    #     pass
+    #
+    # def testResponsive(self):
+    #     '''tests that page switches to mobile view below 768px'''
+    #     pass
+
+    # def tearDown(self):
+    #     self.driver.quit()
+
+
+# class PortfolioTestFirefox(unittest.TestCase):
+#
+#     def setUp(self):
+#         pass
+#
+#     def tearDown(self):
+#         pass
+#
+#
+# class PortfolioTestSafari(unittest.TestCase):
+#
+#     def setUp(self):
+#         pass
+#
+#     def tearDown(self):
+#         pass
+#
+#
+# class PortfolioTestOpera(unittest.TestCase):
+#
+#     def setUp(self):
+#         pass
+#
+#     def tearDown(self):
+#         pass
+#
+#
+# class PortfolioTestIE(unittest.TestCase):
+#
+#     def setUp(self):
+#         pass
+#
+#     def tearDown(self):
+#         pass
+#
+#
+# class PortfolioTestAndroid(unittest.TestCase):
+#
+#     def setUp(self):
+#         pass
+#
+#     def tearDown(self):
+#         pass
+#
+#
+# class PortfolioTestiOS(unittest.TestCase):
+#
+#     def setUp(self):
+#         pass
+#
+#     def tearDown(self):
+#         pass
 
 
 if __name__ == '__main__':
